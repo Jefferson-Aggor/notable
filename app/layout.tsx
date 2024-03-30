baae1c956e1b5a48645624fb51b3afbbbe9432cf
Jefferson-Aggor <aggorjefferson@gmail.com>
2024-03-30 04:44:23 +0000

    Updated to wrap CovexClientProvider around application

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { Toaster } from 'sonner';
 
-import { ClerkProvider } from '@clerk/nextjs'
+import { ConvexClientProvider } from '@/components/providers/convex.provider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -35,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ClerkProvider>
+        <ConvexClientProvider>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
@@ -45,7 +45,8 @@ export default function RootLayout({
             <Toaster position='top-right' />
             {children}
           </ThemeProvider>
-        </ClerkProvider>
+        </ConvexClientProvider>
+
       </body>
     </html>
   );
